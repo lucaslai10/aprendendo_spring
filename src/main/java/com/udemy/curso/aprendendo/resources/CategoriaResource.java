46aeb46bf30833287eae36cb13bcb57fef9a44b9
lucaslai10 <lucaslai10@yahoo.com.br>
2019-06-24 16:28:43 -0300

    Testando classe Categoria

--- src/main/java/com/udemy/curso/aprendendo/resources/CategoriaResource.java
+++ src/main/java/com/udemy/curso/aprendendo/resources/CategoriaResource.java
@@ -1,16 +1,28 @@
 package com.udemy.curso.aprendendo.resources;
 
+import com.udemy.curso.aprendendo.domain.Categoria;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
 
     @GetMapping
-    public String listar(){
-        return "GET funcionando";
+    public List<Categoria> listar(){
+
+        Categoria cat1 = new Categoria(1, "Informática");
+        Categoria cat2 = new Categoria(2, "Escritório");
+
+        List<Categoria> lista = new ArrayList<>();
+        lista.add(cat1);
+        lista.add(cat2);
+
+        return lista;
     }
 
 }
