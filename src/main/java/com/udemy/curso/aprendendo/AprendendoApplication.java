1763d55651aa7e3ea3bb69b2053c27203e8a483f
lucaslai10 <lucaslai10@yahoo.com.br>
2019-06-24 14:38:41 -0300

    projeto aprendendo_spring criado

---
+++ src/main/java/com/udemy/curso/aprendendo/AprendendoApplication.java
@@ -0,0 +1,13 @@
+package com.udemy.curso.aprendendo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AprendendoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AprendendoApplication.class, args);
+	}
+
+}
