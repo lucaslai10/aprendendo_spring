f5512f9ee80df2ff21869b20228a7aa794e012ba
lucaslai10 <lucaslai10@yahoo.com.br>
2019-06-25 16:05:52 -0300

    Produto e associação muito-para-muitos criados

---
+++ src/main/java/com/udemy/curso/aprendendo/repositories/ProdutoRepository.java
@@ -0,0 +1,12 @@
+package com.udemy.curso.aprendendo.repositories;
+
+import com.udemy.curso.aprendendo.domain.Produto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
+
+
+
+}
