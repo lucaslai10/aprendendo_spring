416cec05a7a9f8245a5637c3f0567f659594d33d
lucaslai10 <lucaslai10@yahoo.com.br>
2019-06-26 14:47:29 -0300

    ajustes no endpoint /categorias/{id}

--- src/main/java/com/udemy/curso/aprendendo/services/CategoriaService.java
+++ src/main/java/com/udemy/curso/aprendendo/services/CategoriaService.java
@@ -2,6 +2,7 @@ package com.udemy.curso.aprendendo.services;
 
 import com.udemy.curso.aprendendo.domain.Categoria;
 import com.udemy.curso.aprendendo.repositories.CategoriaRepository;
+import com.udemy.curso.aprendendo.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,6 +14,11 @@ public class CategoriaService {
 
     public Categoria buscar(Integer id){
         Categoria obj = repo.findOne(id);
+        if(obj == null){
+            throw new ObjectNotFoundException("Objeto não encontrado! Id: " + id
+                    + ", Tipo: " + Categoria.class.getName());
+
+        }
         return obj;
     }
 }
