7eb673702903e0093afc4bb344d9c6f2862a58d8
lucaslai10 <lucaslai10@yahoo.com.br>
2019-07-15 14:54:29 -0300

    Inserindo uma nova Categoria com POST

--- src/main/java/com/udemy/curso/aprendendo/domain/Produto.java
+++ src/main/java/com/udemy/curso/aprendendo/domain/Produto.java
@@ -22,8 +22,6 @@ public class Produto implements Serializable {
         joinColumns = @JoinColumn(name = "produto_id"),
         inverseJoinColumns = @JoinColumn(name = "categoria_id")
     )
-
-
     private List<Categoria> categorias = new ArrayList<>();
 
     @JsonIgnore
