254e4eeb15b510b2d8d2878223f67c3d9ca8c22a
lucaslai10 <lucaslai10@yahoo.com.br>
2019-07-16 16:00:01 -0300

    PUT, DELETE e GET para Cliente

--- src/main/java/com/udemy/curso/aprendendo/services/ClienteService.java
+++ src/main/java/com/udemy/curso/aprendendo/services/ClienteService.java
@@ -1,24 +1,65 @@
 package com.udemy.curso.aprendendo.services;
 
 import com.udemy.curso.aprendendo.domain.Cliente;
+import com.udemy.curso.aprendendo.dto.ClienteDTO;
 import com.udemy.curso.aprendendo.repositories.ClienteRepository;
+import com.udemy.curso.aprendendo.services.exceptions.DataIntegrityException;
 import com.udemy.curso.aprendendo.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ClienteService  {
 
     @Autowired
-    private ClienteRepository clienteRepository;
+    private ClienteRepository repo;
 
     public Cliente find(Integer id) {
-        Cliente obj = clienteRepository.findOne(id);
+        Cliente obj = repo.findOne(id);
         if(obj == null) {
             throw new ObjectNotFoundException("Objeto não encontrado! Id: " + id
                     + ", Tipo: " + Cliente.class.getName());
         }
         return obj;
+    }
+
+    public Cliente update(Cliente obj) {
+        Cliente newObj = find(obj.getId());
+        updateData(newObj, obj);
+        return repo.save(newObj);
+    }
+
+    private void updateData(Cliente newObj, Cliente obj) {
+        newObj.setNome(obj.getNome());
+        newObj.setEmail(obj.getEmail());
+    }
+
+    public void delete(Integer id) {
+        find(id);
+        try {
+            repo.delete(id);
+        } catch (DataIntegrityViolationException e) {
+            throw new DataIntegrityException("Não é possível excluir porque há entidades relacionadas");
+        }
+    }
+
+    public List<Cliente> findAll() {
+        return repo.findAll();
+    }
+
+    public Page<Cliente> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+        PageRequest pageRequest = new PageRequest(page, linesPerPage, Sort.Direction.valueOf(direction), orderBy);
+        return repo.findAll(pageRequest);
+    }
+
+    public Cliente fromDTO(ClienteDTO objDto) {
+        return new Cliente(objDto.getId(), objDto.getNome(), objDto. getEmail(), null, null);
     }
 
 }
